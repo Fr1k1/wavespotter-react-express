ef14a4b1fbbb7b235368f3e489d1cdd18b47271c
Martin Friščić <martin.friscic6@gmail.com>
2024-09-12 14:00:23 +0200

    Add tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
-import { getUsers } from "./api/users";
-import { User } from "./common/types";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  const fetchData = async () => {
-    const response = await getUsers();
-    setUsers(response);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <>
-      {users.map((user: User) => {
-        return <h1>{user.first_name}</h1>;
-      })}
+      <h2 className="text-red-500 text-2xl">Homepage</h2>
     </>
   );
 }
