2585f306f1542a609fb6d299a7cf367e392372c8
Martin Friščić <martin.friscic6@gmail.com>
2024-10-09 11:05:00 +0200

    Add functional registration form

--- backend/app/models/user.js
+++ backend/app/models/user.js
@@ -10,7 +10,7 @@ export default (sequelize, DataTypes) => {
       },
       username: { type: DataTypes.STRING(30), allowNull: false, unique: true },
       email: { type: DataTypes.STRING(80), allowNull: false, unique: true },
-      password: { type: DataTypes.STRING(45), allowNull: false },
+      // password: { type: DataTypes.STRING(45), allowNull: false }, supabase stores password in its auth.users table
       first_name: { type: DataTypes.STRING(50), allowNull: false },
       last_name: { type: DataTypes.STRING(50), allowNull: false },
       is_admin: { type: DataTypes.BOOLEAN(), allowNull: false },
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,8 +10,61 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
+import { supabase } from "../supabaseClient";
+import { notifyFailure, notifySuccess } from "@/components/ui/Toast";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { data: authUser, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          first_name: firstName,
+          last_name: lastName,
+          username: username,
+        },
+      },
+    });
+
+    if (error) {
+      notifyFailure("Something went wrong");
+      return;
+    }
+
+    if (authUser) {
+      const { error: insertError } = await supabase.from("users").insert([
+        {
+          id: authUser?.user?.id,
+          username,
+          email,
+          first_name: firstName,
+          last_name: lastName,
+          is_admin: false,
+        },
+      ]);
+
+      if (insertError) {
+        return;
+      }
+
+      notifySuccess("Successfull registration");
+      setFirstName("");
+      setLastName("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
     <div>
       <Card className="w-96 flex flex-col gap-6">
@@ -22,22 +76,54 @@ const Register = () => {
           <form className="mb-4">
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Input id="first_name" placeholder="First name" />
+                <Input
+                  id="first_name"
+                  placeholder="First name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="flex flex-col space-y-1.5">
-                <Input id="last_name" placeholder="Last name" />
+                <Input
+                  id="last_name"
+                  placeholder="Last name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="flex flex-col space-y-1.5">
-                <Input id="username" placeholder="Username" />
+                <Input
+                  id="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Input id="email" placeholder="Email" />
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="flex flex-col space-y-1.5">
-                <Input id="password" placeholder="password" />
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
               </div>
             </div>
           </form>
@@ -45,14 +131,18 @@ const Register = () => {
           <CardDescription>
             or{" "}
             <Link to="/login">
-              {" "}
-              <span className="underline text-secondary"> sign in </span>{" "}
+              <span className="underline text-secondary"> sign in </span>
             </Link>{" "}
             if you already have an account
           </CardDescription>
         </CardContent>
         <CardFooter className="flex w-full">
-          <Button className="w-full" variant={"darker"}>
+          <Button
+            className="w-full"
+            variant={"darker"}
+            type="submit"
+            onClick={handleRegister}
+          >
             Register
           </Button>
         </CardFooter>
