c86e582d21b2a70d502fd3388c45b67aa46d40f3
Martin Friščić <martin.friscic6@gmail.com>
2024-10-09 11:26:24 +0200

    Add functional login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,9 +8,44 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router-dom";
+import { notifyFailure } from "@/components/ui/Toast";
+import { supabase } from "@/supaBaseClient";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { data: authUser, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      notifyFailure("Something went wrong");
+      return;
+    }
+
+    if (authUser) {
+      const { data: userData, error: userError } = await supabase
+        .from("users")
+        .select("is_admin, username, first_name, last_name")
+        .eq("id", authUser.user.id)
+        .single();
+      if (userError) {
+        return;
+      }
+
+      localStorage.setItem("is_admin", userData?.is_admin);
+      navigate("/");
+    }
+  };
   return (
     <div>
       <Card className="w-96 flex flex-col gap-6">
@@ -22,11 +57,23 @@ const Login = () => {
           <form className="mb-4">
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Input id="email" placeholder="Email" />
+                <Input
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="flex flex-col space-y-1.5">
-                <Input id="password" placeholder="password" />
+                <Input
+                  id="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
               </div>
             </div>
           </form>
@@ -41,7 +88,12 @@ const Login = () => {
           </CardDescription>
         </CardContent>
         <CardFooter className="flex w-full">
-          <Button className="w-full" variant={"darker"}>
+          <Button
+            className="w-full"
+            variant={"darker"}
+            type="submit"
+            onClick={handleLogin}
+          >
             Login
           </Button>
         </CardFooter>
