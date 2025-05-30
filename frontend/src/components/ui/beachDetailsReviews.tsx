e9f16691578ff347d686e4740cb5e07942227b16
Martin Friščić <martin.friscic6@gmail.com>
2025-05-30 15:23:45 +0200

    Remove unused import

--- frontend/src/components/ui/beachDetailsReviews.tsx
+++ frontend/src/components/ui/beachDetailsReviews.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Title from "./title";
 import { Button } from "./button";
 import BeachDetailsReviewCard from "./beachDetailsReviewCard";
